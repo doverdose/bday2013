cdd017702bc541c860f529b2f40348ae5b0acd83
Royce Cheng <royce.cheng@gmail.com>
2013-06-04 22:58:26 +0000

    File org try 2

--- public/buildinternet-supersized-2ccedfd/core/js/super-bkgnd.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($){
-	
-	$.supersized({
-		slides  :  	[ {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-1.jpg', title : 'Image Credit: Maria Kazvan'} ]
-	});
-});
